3edc3784f7b57644a4c904531dce888eed07c30f
Jing <boundino@gmail.com>
2017-07-25 01:08:22 -0400

    add filling inv mass

--- dphicor/dphicor_savehist.C
+++ dphicor/dphicor_savehist.C
@@ -5,6 +5,8 @@ int dphicor_savehist(TString infname, TString outfname, TString collisionsyst, I
   std::cout<<std::endl;
   initcutval(collisionsyst);
 
+  for(int i=0;i<=nDphiBins;i++) dphiBins[i] = minDphi+i*(maxDphi-minDphi)/nDphiBins;
+
   TFile* inf = new TFile(infname);
   TTree* ntDkpi = (TTree*)inf->Get("ntDkpi");
   TTree* ntGen = (TTree*)inf->Get("ntGen");
@@ -23,7 +25,9 @@ int dphicor_savehist(TString infname, TString outfname, TString collisionsyst, I
 
   int nentries = ntDkpi->GetEntries();
   TH1D** hdphi = new TH1D*[nhist];
+  TH1D** hmass = new TH1D*[nDphiBins];
   for(int l=0;l<nhist;l++) hdphi[l] = new TH1D(histname[l], ";#Delta#phi;Entries", 50, 0, M_PI);
+  for(int i=0;i<nDphiBins;i++) hmass[i] = new TH1D(Form("hmass_%d",i), ";m_{K#pi} (GeV/c^{2});Entries", 60, 1.7, 2.0);
   for(int i=0;i<nentries;i++)
     {
       if(i%10000==0) xjjuti::progressbar(i, nentries);
@@ -31,7 +35,7 @@ int dphicor_savehist(TString infname, TString outfname, TString collisionsyst, I
       ntHlt->GetEntry(i);
       ntSkim->GetEntry(i);
 
-      if(!val_hlt && !isMC) continue;
+      // if(!val_hlt && !isMC) continue;
       for(int k=0;k<cutval_skim.size();k++)
         {
           if(!val_skim[k]) continue;
@@ -78,8 +82,12 @@ int dphicor_savehist(TString infname, TString outfname, TString collisionsyst, I
           for(std::map<int, float>::iterator it=dphi[l].begin(); it!=dphi[l].end(); it++)
             {
               float deltaphi = TMath::Abs(it->second - dphi[l].at(jleading[l]));
-              float filldeltaphi = deltaphi<M_PI?deltaphi:(2*M_PI-deltaphi);
+              float filldeltaphi = deltaphi<M_PI?deltaphi:(2*M_PI-deltaphi);              
               if(it->first!=jleading[l]) hdphi[l]->Fill(filldeltaphi);
+              if(l) continue;
+              int idphi = xjjuti::findibin(&dphiBins, filldeltaphi);
+              if(idphi<0) return 1;
+              hmass[idphi]->Fill(dcand.Dmass[it->first]);
             }
         }
     }
@@ -88,6 +96,7 @@ int dphicor_savehist(TString infname, TString outfname, TString collisionsyst, I
   TFile* outf = new TFile(Form("%s.root",outfname.Data()),"recreate");
   outf->cd();
   for(int l=0;l<nhist;l++) hdphi[l]->Write();
+  for(int i=0;i<nDphiBins;i++) hmass[i]->Write();
   outf->Write();
   outf->Close();
 
