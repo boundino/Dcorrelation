e69d9a2b509cd1b08651ebc9cf6f1bee562b6c12
Jing <boundino@gmail.com>
2017-09-18 14:02:31 -0400

    add condor for submit.mit.edu

---
+++ condor/skim_dtrk_checkfile.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [[ $# -ne 6 ]]; then
+    echo "usage: ./skim-djet.sh [input file] [output dir] [output filename] [isPP] [isMC] [proxy]"
+    exit 1
+fi
+
+INFILE=$1
+DESTINATION=$2
+OUTFILE=$3
+isPP=$4
+isMC=$5
+export X509_USER_PROXY=${PWD}/$6
+# RESIDUALS=$7
+
+SRM_PREFIX="/mnt/hadoop/"
+SRM_PATH=${DESTINATION#${SRM_PREFIX}}
+
+# tar -xzvf $RESIDUALS
+#FILE=$(head -n$(($1+1)) $2 | tail -n1)
+
+echo ./D_track_skim.exe $INFILE $OUTFILE $isPP $isMC
+./D_track_skim.exe $INFILE $OUTFILE $isPP $isMC
+
+if [[ $? -eq 0 ]]
+then
+    # gfal-copy file://${PWD}/${OUTFILE}  srm://se01.cmsaf.mit.edu:8443/srm/v2/server?SFN=${DESTINATION}/${OUTFILE}
+    gfal-copy file://$PWD/${OUTFILE} gsiftp://se01.cmsaf.mit.edu:2811/${SRM_PATH}/${OUTFILE}
+fi
